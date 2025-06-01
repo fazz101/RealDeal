46d86b3ef0010c8b81e83b81409607a1857cb112
fazz101 <flowmaticaisolutions@outlook.com>
2025-06-01 08:59:21 +0100

    Set up React application files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>R.O.B Hair Restoration</h1>
+      <p>Advanced Hair Transplant Solutions</p>
+    </div>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
-export default { server: { open: true } }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true
+  }
+})
