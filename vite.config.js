861cb40cd9cd7aa58b3f01fd592c672ee8bd1a01
fazz101 <flowmaticaisolutions@outlook.com>
2025-06-01 13:50:14 +0100

    Start the development server

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,1 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/', // Change to '/your-repo-name/' if deploying to GitHub Pages
-  build: {
-    outDir: 'dist', // Netlify/StackBlitz expect this
-    emptyOutDir: true, // Clears old files on build
-  },
-  server: {
-    open: true // Optional: Opens browser on dev server start
-  }
-});
+export default { server: { open: true } }
